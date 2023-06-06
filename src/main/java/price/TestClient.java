5c6b1659f21fea0ff5124c0cb7d9d92917b625a5
tadeusz-cirocki <tadeusz.cirocki@widelab.co>
2023-06-06 15:53:25 +0200

    update comment

--- src/main/java/price/TestClient.java
+++ src/main/java/price/TestClient.java
@@ -25,7 +25,7 @@ public class TestClient
         priceClient.getLatestPrice("GBP/USD");
 
         /*
-            Output (works as expected):
+            Output:
             Published price: Price{id=106, instrument='EUR/USD', bid=1.0989, ask=1.2012, timestamp='01-06-2020 12:01:01:001'}
             Published price: Price{id=110, instrument='EUR/JPY', bid=119.49039, ask=120.02991, timestamp='01-06-2020 12:01:02:110'}
             Published price: Price{id=109, instrument='GBP/USD', bid=1.2486501, ask=1.2573561, timestamp='01-06-2020 12:01:02:100'}
